d6aa4989df6ba3aab83bbc32496ba768e33da2f7
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-13 16:26:12 +0300

    Fix websocket routing path

--- bingo/routing.py
+++ bingo/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/bingo/(?P<room_name>\w+)/$', consumers.BingoConsumer.as_asgi()),
+    re_path(r'^ws/bingo/$', consumers.BingoConsumer.as_asgi()), # ወይም r'ws/bingo/' ከሆነ ከተለያዩ አፃፃፎች ጋር እንዲሄድ ከታች ያለውን ተጠቀም
 ]
