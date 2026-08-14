07c9b47c1857f8f8a2fe965171043e9dbe70da69
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-14 09:30:50 +0300

    Add root redirect to bingo app

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # ይህን Import አድርግ
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bingo/', include('bingo.urls')),  # የ bingo አፕን URL እዚህ እናገናኛለን
+    path('bingo/', include('bingo.urls')),
+    
+    # ባዶ ገፅ (/) ሲከፈት በቀጥታ ወደ /bingo/ እንዲሄድ ያደርጋል
+    path('', RedirectView.as_view(url='/bingo/', permanent=False)),
 ]
