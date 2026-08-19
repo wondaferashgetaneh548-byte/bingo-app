3745553b211f3ce45034a4eb709b5d383bdf2117
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-19 09:11:07 +0300

    Fix KeyError in consumer connect method

--- bingo/routing.py
+++ bingo/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/bingo/(?P<room_name>\w+)/$', consumers.BingoRoomConsumer.as_asgi()),
+    re_path(r'ws/bingo/(?P<room_name>\w+)/$', consumers.BingoConsumer.as_asgi()),
 ]
