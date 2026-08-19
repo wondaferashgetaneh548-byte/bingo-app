cbfc7479b2fc262a16863f16983b969863fd5161
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-19 09:50:26 +0300

    Fix KeyError in consumer connect method

--- bingo/routing.py
+++ bingo/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/bingo/(?P<room_name>\w+)/$', consumers.BingoConsumer.as_asgi()),
+    # BingoConsumer የነበረውን ወደ BingoRoomConsumer ቀይረነዋል
+    re_path(r'ws/bingo/(?P<room_name>\w+)/$', consumers.BingoRoomConsumer.as_asgi()),
 ]
--- core/asgi.py
+++ core/asgi.py
@@ -2,15 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import bingo.routing  # 'bingo' የሚለውን በእውነተኛው የApp ስምህ ተካው
+import bingo.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')  # 'core' የሚለውን በፕሮጀክትህ ስም ተካው
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            bingo.routing.websocket_urlpatterns  # የApp ስምህ .routing
+            bingo.routing.websocket_urlpatterns
         )
     ),
 })
