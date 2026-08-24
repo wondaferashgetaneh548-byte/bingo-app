ffb33f5d4c5be8c7727d5048d590c30bf9b9d29f
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-24 14:02:11 +0300

    Add auto create superuser command

---
+++ bingo/management/commands/create_admin.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'Admin12345!')
+            self.stdout.write(self.style.SUCCESS('Superuser admin created successfully!'))
+        else:
+            self.stdout.write('Superuser admin already exists.')
