c5009f82d8658a14fc2868cf968ade5ca7ece49f
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-24 11:16:18 +0300

    Force DEBUG True to see exception

--- core/settings.py
+++ core/settings.py
@@ -12,8 +12,7 @@ SECURE_SETTINGS = {
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
