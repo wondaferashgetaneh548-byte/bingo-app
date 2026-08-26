971253872c71c831c5bd56e22c7dec09e46ab318
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-26 10:50:08 +0300

    Fix URL routes to load index first and support integer stakes

--- bingo/urls.py
+++ bingo/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.rooms_list, name='rooms_list'),
-    path('index/', views.index, name='index'),
+    # 1. መጀመሪያ የሚከፈተው (Login Page / index.html)
+    path('', views.index, name='index'),
     
-    # 1. በቁጥር የሚጠራ (ምሳሌ፦ /bingo/room/100/)
+    # 2. የ Stake/ብር መጠን መምረጫ (rooms_list.html)
+    path('rooms/', views.rooms_list, name='rooms_list'),
+    
+    # 3. በቁጥር የሚጠራ (ምሳሌ፦ /room/10/)
     path('room/<int:stake>/', views.room_detail, name='room_detail'),
     
-    # 2. በስም የሚጠራ (ምሳሌ፦ /bingo/room_100/)
+    # 4. በስም የሚጠራ (ምሳሌ፦ /room_10/)
     path('room_<str:room_name>/', views.room_detail_by_name, name='room_detail_by_name'),
     
     path('select-card/', views.select_card, name='select_card'),
