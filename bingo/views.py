3c2d332cde9c917674d8e0eb726e4d834010c172
wondaferashgetaneh548-byte <your_email@gmail.com>
2026-08-26 10:17:39 +0300

    Fix template path to bingo/room.html and finish views.py

--- bingo/views.py
+++ bingo/views.py
@@ -1,4 +1,4 @@
- from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 import json
